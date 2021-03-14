e8fbe2429eaa882b1049baaabaabb63947d18085
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2021-03-14 18:28:20 +0100

    ristorantinoFinito

--- ristorantino/main.go
+++ ristorantino/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
 // Piatto ...
@@ -14,31 +17,62 @@ type Cameriere struct {
 	nome string
 }
 
-func ordina(p *Piatto, ch chan Piatto) {
-	ch <- *p
+func ordina(p *Piatto, cook chan Piatto, wg *sync.WaitGroup) {
+	cook <- *p
+	fmt.Println(p.nome, "ordinato")
+	wg.Done()
 }
 
-func cucina() {
+func cucina(cook chan Piatto, deliver chan Piatto, wg *sync.WaitGroup) {
+	for len(cook) > 0 {
+		a := <-cook
+		time.Sleep(time.Duration(4+rand.Intn(3)) * time.Second)
+		deliver <- a
+		fmt.Println(a.nome, "cucinato")
+	}
 
+	wg.Done()
 }
 
-func consegna(wg *sync.WaitGroup) {
-	defer wg.Done()
+func consegna(c *Cameriere, cook chan Piatto, deliver chan Piatto, wg *sync.WaitGroup) {
+
+	for len(cook) > 0 {
+		for len(deliver) > 0 {
+			b := <-deliver
+			time.Sleep(3 * time.Second)
+			fmt.Println(b.nome, "consegnato")
+		}
+		time.Sleep(time.Second)
+	}
+	wg.Done()
 }
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	var wg sync.WaitGroup
 	wg.Add(10)
-	ch := make(chan Piatto, 10)
 
 	piatti := []Piatto{{"A"}, {"B"}, {"C"}, {"D"}, {"E"}, {"F"}, {"G"}, {"H"}, {"I"}, {"J"}}
+	camerieri := []Cameriere{{"Alberto"}, {"Beppe"}}
+	//Ordinazioni
+	cook := make(chan Piatto, 10)
+	for i := range piatti {
+		go ordina(&piatti[i], cook, &wg)
+	}
 
-	for i := 0; i < 10; i++ {
-		func() {
-			go ordina(&piatti[i], ch)
-			go cucina()
-			go consegna(&wg)
-		}()
+	wg.Wait()
+
+	wg.Add(7)
+
+	//Cucina
+	deliver := make(chan Piatto, 10)
+	for i := 0; i < 3; i++ {
+		go cucina(cook, deliver, &wg)
+	}
+
+	//Consegna
+	for i := range camerieri {
+		go consegna(&camerieri[i], cook, deliver, &wg)
 	}
 
 	wg.Wait()
