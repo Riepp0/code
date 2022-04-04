9275a349724f19452938f522ff419fdc02d675d6
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-04-04 18:06:38 +0200

    Esercizi introduttivi

---
+++ Python/name.py
@@ -0,0 +1,10 @@
+#Ask what's your name
+name = input("What's your name? ")
+#Print name type
+print("Your name is a", type(name))
+#print name length
+print("Your name is", len(name), "characters long")
+#print first character
+print("Your first character is", name[0])
+#print last character
+print("Your last character is", name[-1])
---
+++ Python/rectangle.py
@@ -0,0 +1,12 @@
+#Insert rectangle base based on user input
+base = int(input("Enter the base of the rectangle: "))
+#Insert recangle height based on user input
+height = int(input("Enter the height of the rectangle: "))
+#Calculate area of rectangle
+area = base * height
+#Calculate perimeter of rectangle
+perimeter = 2 * (base + height)
+#Print area and perimeter of rectangle
+print("The area of the rectangle is: ", area)
+print("The perimeter of the rectangle is: ", perimeter)
+#End of program
