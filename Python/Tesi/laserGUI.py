8d05b672d280db04870c117feb11b88261b932d1
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-04-14 19:06:40 +0200

    First commit

---
+++ Python/Tesi/laserGUI.py
@@ -0,0 +1,46 @@
+from distutils.command.config import config
+import glob, os
+import tkinter as tk
+import PIL.Image, PIL.ImageTk
+from tkinter import *
+from tkinter import font
+
+### Create object
+root = tk.Tk()
+
+### Add title
+root.title("Laser GUI")
+
+### Dimension
+root.geometry("800x600")
+
+
+### Create label 
+is_on = False
+my_label = Label(root, text = "Laser is off", fg="red", font = ("Helvetica", 14))
+my_label.pack(pady = 10)
+
+### Switch function
+def switch():
+    global is_on
+
+    if is_on:
+        on_button:config(image = off)
+        my_label.config(text = "Laser is off", fg="grey")
+        is_on = True
+    else:
+        on_button:config(image = on)
+        my_label.config(text = "Laser is on", fg="green")
+        is_on = False
+### Define images
+imon = PIL.Image.open("off.png")
+on = PIL.ImageTk.PhotoImage(imon)
+imoff = PIL.Image.open("./img/on.png")
+off = PIL.ImageTk.PhotoImage(imoff)
+
+### Button
+on_button = Button(root, image = on, bd = 0, command = switch)
+on_button.pack(pady = 10)
+
+### Execute
+root.mainloop()
