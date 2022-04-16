45140fd3fb35f47044b9f2bc66cfd285b4740830
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-04-16 12:50:27 +0200

    Switch working

--- Python/Tesi/laserGUI.py
+++ Python/Tesi/laserGUI.py
@@ -1,12 +1,12 @@
 from distutils.command.config import config
 import glob, os
-import tkinter as tk
 from tkinter import *
+from tkinter.ttk import *
 from PIL import Image, ImageFont, ImageDraw, ImageTk
 
 
 ### Create object
-root = tk.Tk()
+root = Tk()
 
 ### Add title
 root.title("Laser GUI")
@@ -14,14 +14,10 @@ root.title("Laser GUI")
 ### Dimension
 root.geometry("800x600")
 
-is_on = True
+is_on = False
 
 ### Create label
-my_label = Label(root,
-    text = "The Switch Is On!",
-    fg = "green",
-    font = ("Helvetica", 14))
- 
+my_label = Label(root, text = "The Switch Is On", font = ("Helvetica", 14))
 my_label.pack(pady = 20)
 
 ### Switch function
@@ -30,24 +26,25 @@ def switch():
      
     # Determine is on or off
     if is_on:
-        on_button.config(image = off)
-        my_label.config(text = "The Switch is Off",
-                        fg = "grey")
+        on_button.config(image = on)
+        my_label.config(text = "The Switch is On")
         is_on = False
     else:
-       
-        on_button.config(image = on)
-        my_label.config(text = "The Switch is On", fg = "green")
+        on_button.config(image = off)
+        my_label.config(text = "The Switch is Off")
         is_on = True
 
 ### Images
-on = PhotoImage(file = "on.png")
-off = PhotoImage(file = "off.png")
+on = PhotoImage(file = r"D:\code\code\Python\Tesi\on.png")
+off = PhotoImage(file = r"D:\code\code\Python\Tesi\off.png")
 
-# Create A Button
-on_button = Button(root, image = on, bd = 0,
-                   command = switch)
-on_button.pack(pady = 50)
+### Create A Button
+on_button = Button(root, image = on, command = switch)
+on_button.pack(pady = 20)
+
+### Create an edittext
+#my_entry = Entry(root, font=("Helvetica", 14))
+#my_entry.pack(pady = 20)
 
 ### Execute
 root.mainloop()
