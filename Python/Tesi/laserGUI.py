2ad743831f31d4daf2a1054767039c645f5b1d5d
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-04-16 19:52:44 +0200

    Good changes tho

--- Python/Tesi/laserGUI.py
+++ Python/Tesi/laserGUI.py
@@ -1,8 +1,15 @@
 from distutils.command.config import config
-import glob, os
 from tkinter import *
 from tkinter.ttk import *
 from PIL import Image, ImageFont, ImageDraw, ImageTk
+import glob,os
+
+
+LOWPOWERLIMIT = 40
+HIGHPOWERLIMIT = 70
+LOWTEMPERATURELIMIT = 20
+HIGHTEMPERATURELIMIT = 40
+SLEEP = 5
 
 
 ### Create object
@@ -41,22 +48,66 @@ off = PhotoImage(file = r"D:\code\code\Python\Tesi\off.png")
 ### Create A Button
 on_button = Button(root, image = off, command = switch)
 on_button.grid(row = 1, column = 0)
+##########################################################################################################
 
 ### Create label
-my_power = Label(root, text = "Insert power (mA)", font = ("Helvetica", 14))
+my_power = Label(root, text = "Insert power", font = ("Helvetica", 14))
 my_power.grid(row = 2, column = 0)
+my_mA = Label(root, text = "mA", font = ("Helvetica", 14))
+my_mA.grid(row = 2, column = 2)
 
 ### Create edit text
 power_text = Entry(root, width = 10)
 power_text.grid(row = 2, column = 1)
 
+### Check if power is a good number
+# Create error label
+power_err = Label(root, font = ("Helvetica", 14))
+power_err.grid(row = 2, column = 4)
+def check_power():
+    try:
+        power = int(power_text.get())
+        if power < LOWPOWERLIMIT or power > HIGHPOWERLIMIT:
+            raise ValueError
+    except ValueError:
+        power_err.config(text = "Insert a valid number!")
+    else:
+        power_err.config(text = "Power set!")
+    
+
+### Create set button
+set_power = Button(root, text = "Set", command = check_power)
+set_power.grid(row = 2, column = 3)
+##########################################################################################################
+
 ### Create label 
-my_temp = Label(root, text = "Insert temperature (°C)", font = ("Helvetica", 14))
+my_temp = Label(root, text = "Insert temperature", font = ("Helvetica", 14))
 my_temp.grid(row = 3, column = 0)
+my_C = Label(root, text = "°C", font = ("Helvetica", 14))
+my_C.grid(row = 3, column = 2)
 
 ### Create edit text
 temp_text = Entry(root, width = 10)
 temp_text.grid(row = 3, column = 1)
+
+### Check if temperature is a good number
+# Create error label
+temp_err = Label(root, font = ("Helvetica", 14))
+temp_err.grid(row = 3, column = 4)
+def check_temp():
+    try:
+        temp = int(temp_text.get())
+        if temp < LOWTEMPERATURELIMIT or temp > HIGHTEMPERATURELIMIT:
+            raise ValueError
+    except ValueError:
+        temp_err.config(text = "Insert a valid number!")
+    else:
+        temp_err.config(text = "Temperature set!")
+
+# Create set button
+set_temp = Button(root, text = "Set", command = check_temp)
+set_temp.grid(row = 3, column = 3)
+##########################################################################################################
 
 
 ### Execute
