b389bd670f53402a6a84a068a3f60f4206435786
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-04-22 00:39:49 +0200

    Modifiche alla libreria

--- Python/Tesi/laserGUI.py
+++ Python/Tesi/laserGUI.py
@@ -1,15 +1,8 @@
-from distutils.command.config import config
 from tkinter import *
 from serial import *
 from PIL import *
 from laserLib import *
 
-
-
-LOWCURRENTLIMIT = 40
-HIGHCURRENTLIMIT = 50
-LOWTEMPERATURELIMIT = 20
-HIGHTEMPERATURELIMIT = 25
 
 laser = LaserBox()
 
@@ -28,35 +21,10 @@ is_on = True
 my_label = Label(root, text="The laser is OFF", font=("Helvetica", 14))
 my_label.grid(row=0, column=0, columnspan=1)
 
-### Switch function
-def switch():
-    global is_on
-
-    # Determine is on or off
-    if is_on:
-        on_button.config(image=off)
-        my_label.config(text="The laser is OFF")
-        # TURN LASER OFF THRUGH COMMAND
-        laser.powerOff()
-        off_label = Label(root, text=laser.readline().decode("utf-8"), font=("Helvetica", 14))
-        off_label.grid(row=6, column=0)
-        is_on = False
-    else:
-        on_button.config(image=on)
-        my_label.config(text="The laser is ON")
-        # TURN LASER ON THRUGH COMMAND
-        laser.powerOn()
-        on_label = Label(root, text=laser.readline().decode("utf-8"), font=("Helvetica", 14))
-        on_label.grid(row=7, column=0)
-        is_on = True
-
-
-### Images
-on = PhotoImage(file=r"C:\Users\miche\source\vscode\code\Python\Tesi\on.png")
-off = PhotoImage(file=r"C:\Users\miche\source\vscode\code\Python\Tesi\off.png")
 
 ### Create A Button (SWitch)
-on_button = Button(root, image=off, command=switch)
+on_button = Button(root, image=off)
+on_button.config(command=laser.switch(on_button, my_label))
 on_button.grid(row=1, column=0)
 ##########################################################################################################
 
@@ -70,32 +38,12 @@ my_mA.grid(row=2, column=2)
 current_text = Entry(root, width=10)
 current_text.grid(row=2, column=1)
 
-### Check if current is a good number
 # Create error label
 current_err = Label(root, font=("Helvetica", 14))
 current_err.grid(row=2, column=4)
 
-
-def check_current():
-    if is_on == False:
-        current_err.config(text="The laser is OFF")
-        return
-    try:
-        current = float(current_text.get())
-        print(current_text.get())
-        if current < LOWCURRENTLIMIT or current > HIGHCURRENTLIMIT:
-            raise ValueError
-    except ValueError:
-        current_err.config(text="Insert a valid number!")
-    else:
-        current_err.config(text="Current set!")
-        # SET LASER current THRUGH COMMAND
-        laser.write(("slc:"+current_text.get()+"\r\n").encode())
-        laser.readline().decode("utf-8")
-
-
 ### Create set button
-set_current = Button(root, text="Set", command=check_current)
+set_current = Button(root, text="Set", command=laser.checkCurrent(is_on, current_err, current_text))
 set_current.grid(row=2, column=3)
 ##########################################################################################################
 
@@ -109,50 +57,23 @@ my_C.grid(row=3, column=2)
 temp_text = Entry(root, width=10)
 temp_text.grid(row=3, column=1)
 
-### Check if temperature is a good number
 # Create error label
 temp_err = Label(root, font=("Helvetica", 14))
 temp_err.grid(row=3, column=4)
 
-
-def check_temp():
-    if is_on == False:
-        temp_err.config(text="The laser is OFF")
-        return
-    try:
-        temp = float(temp_text.get())
-        if temp < LOWTEMPERATURELIMIT or temp > HIGHTEMPERATURELIMIT:
-            raise ValueError
-    except ValueError:
-        temp_err.config(text="Insert a valid number!")
-    else:
-        temp_err.config(text="Temperature set!")
-        # SET LASER TEMPERATURE THRUGH COMMAND
-        laser.write(("stt:"+temp_text.get()+"\r\n").encode())
-        laser.readline().decode("utf-8")
-
-
 # Create set button
-set_temp = Button(root, text="Set", command=check_temp)
+set_temp = Button(root, text="Set", command=laser.checkCurrent(is_on, temp_err, temp_text))
 set_temp.grid(row=3, column=3)
 ##########################################################################################################
 
 ### GET LASER CURRENT CURRENT THRUGH COMMAND
-def getCurrent():
-    laser.write("rli?\r\n".encode())
-    my_actual_temp = Label(root, text="Actual current: " + laser.readline().decode("utf-8") , font=("Helvetica", 14))
-    my_actual_temp.grid(row=4, column=1)
 
-get_current = Button(root, text="Get current", command=getCurrent)
+get_current = Button(root, text="Get current", command=laser.getCurrent(root))
 get_current.grid(row=4, column=0)
 
 ### GET LASER CURRENT TEMPERATURE THRUGH COMMAND
-def getTemp():
-    laser.write("rtt?\r\n".encode())
-    my_current_temp = Label(root, text="Actual temperature: " + laser.readline().decode("utf-8"), font=("Helvetica", 14))
-    my_current_temp.grid(row=5, column=1)
 
-get_temp = Button(root, text="Actual temperature", command=getTemp)
+get_temp = Button(root, text="Actual temperature", command=laser.getTemp(root))
 get_temp.grid(row=5, column=0)
 
 
--- Python/Tesi/laserLib.py
+++ Python/Tesi/laserLib.py
@@ -1,19 +1,108 @@
-from socket import timeout
+from tkinter import *
 from serial import *
+from PIL import *
 
 class LaserBox:
     """ Creating a laser class """
 
-    global laser
+    global laser, on, off
+
+    ### Images
+    on = PhotoImage(file=r"C:\Users\miche\source\vscode\code\Python\Tesi\on.png")
+    off = PhotoImage(file=r"C:\Users\miche\source\vscode\code\Python\Tesi\off.png")
 
 
     def __init__() -> None:
         """ Initialize laser """
+
         laser = Serial("COM5", timeout=3)  
     
     def powerOn():
+        """ Power on laser """
+
         laser.write("lon\r\n".encode())
+        laser.readline().decode("utf-8")
 
     def powerOff():
+        """ Power off laser """
+
         laser.write("loff\r\n".encode())
+        laser.readline().decode("utf-8")
+    
+    def switch(on_button, my_label):
+        """ Switch laser ON/OFF """
+
+        if is_on:
+            on_button.config(image=off)
+            my_label.config(text="The laser is OFF")
+            # TURN LASER OFF THRUGH COMMAND
+            laser.powerOff()
+            is_on = False
+        else:
+            on_button.config(image=on)
+            my_label.config(text="The laser is ON")
+            # TURN LASER ON THRUGH COMMAND
+            laser.powerOn()
+            is_on = True
+
+    def setCurrent(current):
+        """ Set current through serial command """
+
+        laser.write(("slc:"+current+"\r\n").encode())
+        laser.readline().decode("utf-8")
+
+    def setTemp(temp):
+        """ Set temperature through serial command """
+
+        laser.write(("stt:"+temp+"\r\n").encode())
+        laser.readline().decode("utf-8")
+    
+    def getCurrent(root):
+        """ Get current through serial command """
+
+        laser.write("rli?\r\n".encode())
+        my_actual_temp = Label(root, text="Actual current: " + laser.readline().decode("utf-8") , font=("Helvetica", 14))
+        my_actual_temp.grid(row=4, column=1)
+
+    def getTemp(root):
+        """ Get temperature through serial command """
+
+        laser.write("rtt?\r\n".encode())
+        my_current_temp = Label(root, text="Actual temperature: " + laser.readline().decode("utf-8"), font=("Helvetica", 14))
+        my_current_temp.grid(row=5, column=1)
+    
+    def checkCurrent(is_on, current_err, current_text):
+        """ Check if current is a valid number """
+
+        if is_on == False:
+            current_err.config(text="The laser is OFF")
+            return
+        try:
+            current = float(current_text.get())
+            if current < 40 or current > 50:
+                raise ValueError
+        except ValueError:
+            current_err.config(text="Insert a valid number!")
+        else:
+            current_err.config(text="Current set!")
+            laser.setCurrent(current)
+
+    def checkTemp(is_on, temp_err, temp_text):
+        """ Check if temperature is a valid number """
+
+        if is_on == False:
+            temp_err.config(text="The laser is OFF")
+            return
+        try:
+            temp = float(temp_text.get())
+            if temp < 20 or temp > 25:
+                raise ValueError
+        except ValueError:
+            temp_err.config(text="Insert a valid number!")
+        else:
+            temp_err.config(text="Temperature set!")
+            laser.setTemp(temp)
+
+    
+
 
