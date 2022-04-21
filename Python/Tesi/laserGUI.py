f877bd262307f8c3ce1d3e6a441626e1b1b05c5b
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-04-21 10:49:54 +0200

    Laser current status

--- Python/Tesi/laserGUI.py
+++ Python/Tesi/laserGUI.py
@@ -2,7 +2,7 @@ from distutils.command.config import config
 from tkinter import *
 from tkinter.ttk import *
 from PIL import Image, ImageFont, ImageDraw, ImageTk
-import glob,os
+import glob, os
 
 
 LOWPOWERLIMIT = 40
@@ -24,100 +24,114 @@ root.geometry("800x600")
 is_on = False
 
 ### Create label + button
-my_label = Label(root, text = "The laser is OFF", font = ("Helvetica", 14))
-my_label.grid(row = 0, column = 0, columnspan = 1)
+my_label = Label(root, text="The laser is OFF", font=("Helvetica", 14))
+my_label.grid(row=0, column=0, columnspan=1)
 
 ### Switch function
 def switch():
     global is_on
-     
+
     # Determine is on or off
     if is_on:
-        on_button.config(image = off)
-        my_label.config(text = "The laser is OFF")
+        on_button.config(image=off)
+        my_label.config(text="The laser is OFF")
         # TURN LASER OFF THRUGH COMMAND
         is_on = False
     else:
-        on_button.config(image = on)
-        my_label.config(text = "The laser is ON")
+        on_button.config(image=on)
+        my_label.config(text="The laser is ON")
         # TURN LASER ON THRUGH COMMAND
         is_on = True
 
+
 ### Images
-on = PhotoImage(file = r"C:\Users\miche\source\vscode\code\Python\Tesi\on.png")
-off = PhotoImage(file = r"C:\Users\miche\source\vscode\code\Python\Tesi\off.png")
+on = PhotoImage(file=r"C:\Users\miche\source\vscode\code\Python\Tesi\on.png")
+off = PhotoImage(file=r"C:\Users\miche\source\vscode\code\Python\Tesi\off.png")
 
 ### Create A Button (SWitch)
-on_button = Button(root, image = off, command = switch)
-on_button.grid(row = 1, column = 0)
+on_button = Button(root, image=off, command=switch)
+on_button.grid(row=1, column=0)
 ##########################################################################################################
 
 ### Create label
-my_power = Label(root, text = "Insert power", font = ("Helvetica", 14))
-my_power.grid(row = 2, column = 0)
-my_mA = Label(root, text = "mA", font = ("Helvetica", 14))
-my_mA.grid(row = 2, column = 2)
+my_power = Label(root, text="Insert power", font=("Helvetica", 14))
+my_power.grid(row=2, column=0)
+my_mA = Label(root, text="mA", font=("Helvetica", 14))
+my_mA.grid(row=2, column=2)
 
 ### Create edit text
-power_text = Entry(root, width = 10)
-power_text.grid(row = 2, column = 1)
+power_text = Entry(root, width=10)
+power_text.grid(row=2, column=1)
 
 ### Check if power is a good number
 # Create error label
-power_err = Label(root, font = ("Helvetica", 14))
-power_err.grid(row = 2, column = 4)
+power_err = Label(root, font=("Helvetica", 14))
+power_err.grid(row=2, column=4)
+
+
 def check_power():
     if is_on == False:
-        power_err.config(text = "The laser is OFF")
+        power_err.config(text="The laser is OFF")
         return
     try:
         power = int(power_text.get())
         if power < LOWPOWERLIMIT or power > HIGHPOWERLIMIT:
             raise ValueError
     except ValueError:
-        power_err.config(text = "Insert a valid number!")
+        power_err.config(text="Insert a valid number!")
     else:
-        power_err.config(text = "Power set!")
+        power_err.config(text="Power set!")
         # SET LASER POWER THRUGH COMMAND
-    
+
 
 ### Create set button
-set_power = Button(root, text = "Set", command = check_power)
-set_power.grid(row = 2, column = 3)
+set_power = Button(root, text="Set", command=check_power)
+set_power.grid(row=2, column=3)
 ##########################################################################################################
 
-### Create label 
-my_temp = Label(root, text = "Insert temperature", font = ("Helvetica", 14))
-my_temp.grid(row = 3, column = 0)
-my_C = Label(root, text = "°C", font = ("Helvetica", 14))
-my_C.grid(row = 3, column = 2)
+### Create label
+my_temp = Label(root, text="Insert temperature", font=("Helvetica", 14))
+my_temp.grid(row=3, column=0)
+my_C = Label(root, text="°C", font=("Helvetica", 14))
+my_C.grid(row=3, column=2)
 
 ### Create edit text
-temp_text = Entry(root, width = 10)
-temp_text.grid(row = 3, column = 1)
+temp_text = Entry(root, width=10)
+temp_text.grid(row=3, column=1)
 
 ### Check if temperature is a good number
 # Create error label
-temp_err = Label(root, font = ("Helvetica", 14))
-temp_err.grid(row = 3, column = 4)
+temp_err = Label(root, font=("Helvetica", 14))
+temp_err.grid(row=3, column=4)
+
+
 def check_temp():
     if is_on == False:
-        temp_err.config(text = "The laser is OFF")
+        temp_err.config(text="The laser is OFF")
         return
     try:
         temp = int(temp_text.get())
         if temp < LOWTEMPERATURELIMIT or temp > HIGHTEMPERATURELIMIT:
             raise ValueError
     except ValueError:
-        temp_err.config(text = "Insert a valid number!")
+        temp_err.config(text="Insert a valid number!")
     else:
-        temp_err.config(text = "Temperature set!")
+        temp_err.config(text="Temperature set!")
         # SET LASER TEMPERATURE THRUGH COMMAND
 
+
 # Create set button
-set_temp = Button(root, text = "Set", command = check_temp)
-set_temp.grid(row = 3, column = 3)
+set_temp = Button(root, text="Set", command=check_temp)
+set_temp.grid(row=3, column=3)
 ##########################################################################################################
+
+### GET LASER CURRENT POWER THRUGH COMMAND
+my_current_temp = Label(root, text="Current power: ", font=("Helvetica", 14))
+my_current_temp.grid(row=4, column=0)
+
+### GET LASER CURRENT TEMPERATURE THRUGH COMMAND
+my_current_temp = Label(root, text="Current temperature: ", font=("Helvetica", 14))
+my_current_temp.grid(row=5, column=0)
 
 
 ### Execute
