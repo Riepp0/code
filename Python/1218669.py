164fb4e523fdf6ca60930e0d8526bfa6d1d75d4b
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-05-02 22:54:02 +0200

    Started exercise

--- Python/1218669.py
+++ Python/1218669.py
@@ -1,41 +1,26 @@
-def massimo(a,b):
-    if a == b:
-        return 0
-    elif a > b:
-        return 1
+import networkx as nx 
+
+def diam_graph(grafo : nx.Graph):
+    """    Funzione che calcola il diametro del grafo    """
+    if isinstance(grafo, nx.Graph) and (grafo != None):
+        return nx.diameter(grafo)
     else:
-        return -1
+        return "Tipo di dato non valido"
 
-
-def moltiplicatrice(lista_numeri):
-    if(len(lista_numeri) == 0):
-        return 0
+def max_degree(grafo : nx.Graph):
+    """    Funzione che calcola il massimo grado del grafo    """
+    if isinstance(grafo, nx.Graph) and (grafo != None):
+        return max(grafo.degree())
     else:
-        totale = 1
-        for i in range(len(lista_numeri)):
-            totale = totale * lista_numeri[i]
-        return totale
+        return "Tipo di dato non valido"
 
-def rovescia(stringa):
-    if(len(stringa) == 0):
-        return "Stringa vuota"
-    else:
-        s = stringa.lower()
-        f = s[0].upper()
-        str = f + s[1:]
-        stringa = str[::-1]
-        return stringa
-
-def frequenza(stringa):
-    if(len(stringa) == 0):
-        return "Stringa vuota"
-    else:
-        diz = {}
-        for i in range(len(stringa)):
-            if(stringa[i] in diz):
-                diz[stringa[i]] = diz[stringa[i]] + 1
-            else:
-                diz[stringa[i]] = 1
-        return diz
-
+def ampiezza(grafo : nx.Graph, start, end):
+    """    Funzione che calcola l'ampiezza del grafo da start ad end"""
+    # Check if grafo is a valid graph
+    if isinstance(grafo, nx.Graph) and (grafo != None):
+        # Check if start and end are valid nodes
+        if start in grafo.nodes() and end in grafo.nodes():
+            return nx.shortest_path_length(grafo, start, end)
+        return "Nodo non presente nel grafo"
+    return "Tipo di dato non valido"
 
