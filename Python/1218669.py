124357f8c561029ba8e86b2e02e761343f2d89fc
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-05-02 23:43:03 +0200

    Push esotico

--- Python/1218669.py
+++ Python/1218669.py
@@ -1,26 +1,32 @@
 import networkx as nx 
 
-def diam_graph(grafo : nx.Graph):
+#Ok
+def diam_graph(grafo):
     """    Funzione che calcola il diametro del grafo    """
     if isinstance(grafo, nx.Graph) and (grafo != None):
         return nx.diameter(grafo)
     else:
         return "Tipo di dato non valido"
 
-def max_degree(grafo : nx.Graph):
+def max_degree(grafo):
     """    Funzione che calcola il massimo grado del grafo    """
     if isinstance(grafo, nx.Graph) and (grafo != None):
-        return max(grafo.degree())
+        #Returns the list of max degree nodes
+        return max(nx.degree(grafo))
     else:
         return "Tipo di dato non valido"
 
-def ampiezza(grafo : nx.Graph, start, end):
+def ampiezza(grafo, start, end):
     """    Funzione che calcola l'ampiezza del grafo da start ad end"""
     # Check if grafo is a valid graph
     if isinstance(grafo, nx.Graph) and (grafo != None):
         # Check if start and end are valid nodes
         if start in grafo.nodes() and end in grafo.nodes():
-            return nx.shortest_path_length(grafo, start, end)
+            #Returns the list of visited nodes from start to end
+            return nx.shortest_path(grafo, start, end)
         return "Nodo non presente nel grafo"
     return "Tipo di dato non valido"
+
+
+
 
