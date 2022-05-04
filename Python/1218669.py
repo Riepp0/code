8769e0d0d457f418fd890e0eca427c5e9a206d8e
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-05-04 15:42:54 +0200

    Ok max_degree

--- Python/1218669.py
+++ Python/1218669.py
@@ -1,18 +1,23 @@
 import networkx as nx 
 
-#Ok
+# Ok
 def diam_graph(grafo):
     """    Funzione che calcola il diametro del grafo    """
     if isinstance(grafo, nx.Graph) and (grafo != None):
         return nx.diameter(grafo)
     else:
         return "Tipo di dato non valido"
-
+# OK
 def max_degree(grafo):
     """    Funzione che calcola il massimo grado del grafo    """
     if isinstance(grafo, nx.Graph) and (grafo != None):
-        #Returns the list of max degree nodes
-        return max(nx.degree(grafo))
+        grado = 0
+        for i in grafo.nodes():
+            if grafo.degree(i) > grado:
+                grado = grafo.degree(i)
+        #Returns the list of nodes with max degree
+        return [i for i in grafo.nodes() if grafo.degree(i) == grado]
+          
     else:
         return "Tipo di dato non valido"
 
@@ -27,6 +32,9 @@ def ampiezza(grafo, start, end):
         return "Nodo non presente nel grafo"
     return "Tipo di dato non valido"
 
+G = nx.Graph()
+G.add_nodes_from(["A","B","C"])
+G.add_edges_from([("A","B"),("A","C")])
 
 
 
