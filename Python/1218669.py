1a76a69a1117c60072bfe372626b0ca95859c4f8
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-05-04 16:01:47 +0200

    Check ampiezza

--- Python/1218669.py
+++ Python/1218669.py
@@ -3,14 +3,14 @@ import networkx as nx
 # Ok
 def diam_graph(grafo):
     """    Funzione che calcola il diametro del grafo    """
-    if isinstance(grafo, nx.Graph) and (grafo != None):
+    if isinstance(grafo, nx.Graph) and nx.is_empty(grafo):
         return nx.diameter(grafo)
     else:
         return "Tipo di dato non valido"
 # OK
 def max_degree(grafo):
     """    Funzione che calcola il massimo grado del grafo    """
-    if isinstance(grafo, nx.Graph) and (grafo != None):
+    if isinstance(grafo, nx.Graph) and nx.is_empty(grafo):
         grado = 0
         for i in grafo.nodes():
             if grafo.degree(i) > grado:
@@ -23,12 +23,23 @@ def max_degree(grafo):
 
 def ampiezza(grafo, start, end):
     """    Funzione che calcola l'ampiezza del grafo da start ad end"""
-    # Check if grafo is a valid graph
-    if isinstance(grafo, nx.Graph) and (grafo != None):
+    if isinstance(grafo, nx.Graph) and nx.is_empty(grafo):
         # Check if start and end are valid nodes
         if start in grafo.nodes() and end in grafo.nodes():
             #Returns the list of visited nodes from start to end
-            return nx.shortest_path(grafo, start, end)
+            if start == end:
+                return [start]
+            else:
+                visited = []
+                queue = [start]
+                while queue:
+                    nodo = queue.pop()
+                    visited.append(nodo)
+                    for neighbors in grafo.neighbors(nodo):
+                        if neighbors not in visited and neighbors not in queue:
+                            queue[neighbors]
+                return visited
+            
         return "Nodo non presente nel grafo"
     return "Tipo di dato non valido"
 
