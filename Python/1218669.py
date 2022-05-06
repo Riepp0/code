5f1c99e3c355430fc98d5cb644b129d0cf3163e0
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-05-06 12:19:41 +0200

    Ok step ampiezza

--- Python/1218669.py
+++ Python/1218669.py
@@ -1,13 +1,12 @@
 import networkx as nx 
 
-# Ok
 def diam_graph(grafo):
     """    Funzione che calcola il diametro del grafo    """
     if isinstance(grafo, nx.Graph) and grafo != None:
         return nx.diameter(grafo)
     else:
         return "Tipo di dato non valido"
-# OK
+
 def max_degree(grafo):
     """    Funzione che calcola il massimo grado del grafo    """
     if isinstance(grafo, nx.Graph) and grafo != None:
@@ -36,6 +35,8 @@ def ampiezza(grafo, start, end):
                 while queue:
                     nodo = queue.pop()
                     list.append(nodo)
+                    if nodo == end:
+                        break
                     for neighbors in grafo.neighbors(nodo):
                         if neighbors not in list and neighbors not in queue:
                             queue.append(neighbors)
@@ -44,9 +45,9 @@ def ampiezza(grafo, start, end):
         return "Nodo non presente nel grafo"
     return "Tipo di dato non valido"
 
-G = nx.Graph()
-G.add_nodes_from(["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
-G.add_edges_from([("A","B"),("A","C"),("A","D"),("A","E"),("A","F"),("A","G"),("A","H"),("A","I"),("A","J"),("A","K"),("A","L"),("A","M"),("A","N"),("A","O"),("A","P"),("A","Q"),("A","R"),("A","S"),("A","T"),("A","U"),("A","V"),("A","W"),("A","X"),("A","Y"),("A","Z"),("B","C"),("B","D"),("B","E"),("B","F"),("B","G"),("B","H"),("B","I"),("B","J"),("B","K"),("B","L"),("B","M"),("B","N"),("B","O"),("B","P"),("B","Q"),("B","R"),("B","S"),("B","T"),("B","U"),("B","V"),("B","W"),("B","X"),("B","Y"),("B","Z"),("C","D"),("C","E"),("C","F"),("C","G"),("C","H"),("C","I"),("C","J"),("C","K"),("C","L"),("C","M"),("C","N"),("C","O"),("C","P"),("C","Q"),("C","R"),("C","S"),("C","T"),("C","U"),("C","V"),("C","W"),("C","X"),("C","Y"),("C","Z"),("D","E"),("D","F"),("D","G"),("D","H"),("D","I"),("D","J"),("D","K"),("D","L"),("D","M"),("D","N"),("D","O"),("D","P"),("D","Q"),("D","R"),("D","S"),("D","T"),("D","U"),("D","V"),("D","W"),("D","X"),("D","Y"),("D","Z"),("E","F"),("E","G"),("E","H"),("E","I"),("E","J"),("E","K"),("E","L"),("E","M"),])
-print(ampiezza(G, "A", "E"))
+#G = nx.Graph()
+#G.add_nodes_from(["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"])
+#G.add_edges_from([("A","B"),("B", "C"),("C", "D"), ("D", "E"), ("E", "F"), ("F", "G")])
+#print(ampiezza(G, "A", "E"))
 
 
