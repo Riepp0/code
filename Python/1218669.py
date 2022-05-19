938c5f787db7d9887edbe2c86b7d8fbd6a6328a1
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-05-19 19:25:42 +0200

    Conclusione consegna 4 python

--- Python/1218669.py
+++ Python/1218669.py
@@ -1,22 +1,23 @@
 import numpy as np
 import pandas as pd
 
-def compute_cost(X, y, theta):
+# Esercizio 1
+def computeCost(X, y, theta):
   squared_error = np.power(((X * theta.T) - y), 2)
   return np.sum(squared_error) / (2 * len(X))
 
-def train(X, y, theta, alpha, iters):
-  temp = np.matrix(np.zeros(theta.shape))
-  params = int(theta.ravel().shape[1])
+def learn(X, y, theta, alpha, iters):
+  tmp = np.matrix(np.zeros(theta.shape))
+  param = int(theta.ravel().shape[1])
   cost = np.zeros(iters)
 
   for i in range(iters):
     error = (X * theta.T) - y
-    for j in range(params):
+    for j in range(param):
       term = np.multiply(error, X[:,j])
-      temp[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term))
-    theta = temp
-    cost[i] = compute_cost(X, y, theta)
+      tmp[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term))
+    theta = tmp
+    cost[i] = computeCost(X, y, theta)
 
   return theta, cost
 
@@ -24,7 +25,7 @@ def linear_regr(dataset, value):
   alpha = 0.01
   iters = 1000
   try:
-    data = pd.read_csv(dataset, header=None, names=["Population", "Profit"])
+    data = pd.read_csv(dataset, header=None, names=["Col1", "Col2"])
   except FileNotFoundError:
     return None
 
@@ -38,17 +39,19 @@ def linear_regr(dataset, value):
   y = np.matrix(y.values)
   theta = np.matrix(np.array([0, 0]))
 
-  theta, _ = train(X, y, theta, alpha, iters)
+  theta, _ = learn(X, y, theta, alpha, iters)
   output = np.matrix([1, value]) * theta.T
   
   return float(output)
 
+# Esercizio 2
 def vertical_stack(a,b):
     if len(a) != len(b):
         return None
     else:
         return np.vstack((a,b))
 
+# Esercizio 3
 def match(a,b):
     if len(a) != len(b):
         return None
