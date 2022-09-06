d39f1e212f6d45d90996e8bf3c5a06b35f4dfdf0
Michele Rieppi <michele.rieppi@studenti.unipd.it>
2022-09-06 11:12:37 +0200

    test scanner for laser's data

---
+++ Python/Tesi/test_scanner.py
@@ -0,0 +1,33 @@
+import os
+import numpy as np
+import time
+"""
+for filename in os.listdir(os.getcwd()+'\Python\Tesi\CSV\dati'):
+
+    data = np.load(os.getcwd()+'\Python\Tesi\CSV\dati\\'+filename)
+    x = data['x'] # Wavelength
+    y = data['y'] # Intensity
+    max_intensity = np.amax(y) # Max intensity
+    max_index = np.argmax(y) # Index of max intensity
+    max_wavelength = x[max_index] # Wavelength of max intensity
+    counter = 0 # Counter for the number of points
+    sumIntensity = 0 # Sum of the intensity
+    for j in range(10):
+        sumIntensity += y[j]
+    averageIntensity = sumIntensity/10 # Average intensity
+    peakHeight = max_intensity - averageIntensity # Peak height
+    for i in range(len(y)):
+        if y[i] > 0.2*peakHeight + averageIntensity:
+            counter += 1
+    np.savez('Python\Tesi\CSV\max_dati\\'+filename, max_intensity=max_intensity, max_wavelength=max_wavelength, max_index=max_index, counter=counter)
+"""
+#"""
+# test lettura
+for filename in os.listdir(os.getcwd()+ '\Python\Tesi\CSV\max_dati'):
+    data = np.load(os.getcwd()+'\Python\Tesi\CSV\max_dati\\'+filename)
+    print(data['max_intensity'])
+    print(data['max_wavelength'])
+    print(data['max_index'])
+    print(data['counter'])
+    time.sleep(2)
+    #"""
